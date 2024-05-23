b9ba5d16db5a3ec43540357204f80e7527b8239a
yohanan-collab <63300602+yohanan-collab@users.noreply.github.com>
2024-05-23 17:32:06 +0200

    up

---
+++ backend/routes/gameRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const gameController = require('../controllers/gameController');
+
+router.get('/', gameController.getAllGames);
+
+module.exports = router;
